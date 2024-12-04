56e40c7d2f53bcbe04230a399f5ca18c3ca6e0cf
Hizqil Alfi <hizqilhizqil308@gmail.com>
2024-12-04 23:13:03 +0700

    Update CSS

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -17,6 +17,70 @@
         <a href="vote" class="link_content"><p class="text_content">Candidate</p></a>
         </div>
     </div>
+    <div class="countdown">
+        <p id="demo"></p>
+
+        <script>
+        // Set the date we're counting down to
+        var countDownDate = new Date("Dec 10, 2024 17:00:00").getTime();
+        
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+        
+          // Get today's date and time
+          var now = new Date().getTime();
+        
+          // Find the distance between now and the count down date
+          var distance = countDownDate - now;
+        
+          // Time calculations for days, hours, minutes and seconds
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+          // Display the result in the element with id="demo"
+          document.getElementById("demo").innerHTML = hours + ": " + minutes + ": " + seconds + " ";
+        
+          // If the count down is finished, write some text
+          if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+          }
+        }, 1000);
+        </script>
+    </div>
+    <div class="text_v">
+        <h2>Voting</h2>
+    </div>
+    <div class="box_va box_vai">
+        <img src="/img/sampl1.png" alt="" class="cndt">
+        <div class="box_vb">
+            <div class="info">
+                <div class="name">
+                    <p class="name_p">Kandidat 1</p>
+                    <p class="let">Informatika 22</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="btn_v btn_v1">
+        VOTE
+    </button>
+    <div class="box_va box_vbi">
+        <img src="/img/sampl2.png" alt="" class="cndt">
+        <div class="box_vb">
+            <div class="info">
+                <div class="name">
+                    <p class="name_p">Kandidat 2</p>
+                    <p class="let">Informatika 22</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="btn_v btn_v2">
+        VOTE
+    </button>
     <div>
         <img src="/img/footer.png" alt="" class="footer">
     </div>
